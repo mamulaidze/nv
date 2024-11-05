37fa8318589be2e6aabd723a4e3628d0546b787c
mamulaidze <mamulaidzeg186@gmail.com>
2024-11-06 00:06:05 +0400

    version 0.02 (host)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/nv/', // Replace 'your-repo-name' with your actual GitHub repository name
   plugins: [react()],
-})
+});
